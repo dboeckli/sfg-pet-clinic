ddb892de35c91c0676d1ea0282cef3f1aca651a0
dboeckli <Big.fat.cow-2010>
2021-02-01 16:08:44 +0100

    list vets in index - Closes #15

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,14 +1,25 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
