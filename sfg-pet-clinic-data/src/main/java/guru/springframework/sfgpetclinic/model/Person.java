4dd55be255b9e58299f15e8de362bc1b575ed544
dboeckli <dboeckli@gmail.com>
2025-03-01 09:12:40 +0100

    upgrade

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,9 +23,11 @@ public class Person extends BaseEntity {
     }
 
     @Column(name = "first_name")
+    @NotBlank
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank
     private String lastName;
 
 }
