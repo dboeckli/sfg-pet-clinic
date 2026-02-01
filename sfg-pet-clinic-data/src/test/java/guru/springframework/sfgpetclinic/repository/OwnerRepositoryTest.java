22923e37eaa2cc022baeab61e9f53d16550dfab1
Dominique Boeckli <dboeckli@gmail.com>
2026-02-01 16:09:14 +0100

    add unit tests

---
+++ sfg-pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/repository/OwnerRepositoryTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.repository;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@ActiveProfiles("springdatajpa")
+@ContextConfiguration(classes = {OwnerRepositoryTest.TestConfig.class})
+class OwnerRepositoryTest {
+
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    // Minimal configuration needed because no @SpringBootApplication is found
+    @EnableJpaRepositories(basePackages = "guru.springframework.sfgpetclinic.repository")
+    @EntityScan(basePackages = "guru.springframework.sfgpetclinic.model")
+    static class TestConfig {
+    }
+
+    @Test
+    void findByLastName() {
+        Owner owner = Owner.builder()
+            .firstName("Max")
+            .lastName("Mustermann")
+            .address("Musterstraße 1")
+            .city("Berlin")
+            .telephone("123456789")
+            .build();
+
+        ownerRepository.save(owner);
+
+        Owner foundOwner = ownerRepository.findByLastName("Mustermann");
+        assertNotNull(foundOwner);
+        assertEquals("Max", foundOwner.getFirstName());
+    }
+}
