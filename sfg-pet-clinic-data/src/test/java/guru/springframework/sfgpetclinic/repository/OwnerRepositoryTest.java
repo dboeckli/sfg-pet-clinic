e46d4f02cd79e2bb07c9e03f6cb494cda84549ca
Dominique Boeckli <dboeckli@gmail.com>
2026-02-04 07:37:33 +0100

    implement test for map using nested approach

--- sfg-pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/repository/OwnerRepositoryTest.java
+++ sfg-pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/repository/OwnerRepositoryTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.repository;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -12,34 +13,49 @@ import org.springframework.test.context.ContextConfiguration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@DataJpaTest
-@ActiveProfiles("springdatajpa")
-@ContextConfiguration(classes = {OwnerRepositoryTest.TestConfig.class})
 class OwnerRepositoryTest {
 
-    @Autowired
-    OwnerRepository ownerRepository;
+    abstract static class AbstractOwnerServiceTest {
 
-    // Minimal configuration needed because no @SpringBootApplication is found
-    @EnableJpaRepositories(basePackages = "guru.springframework.sfgpetclinic.repository")
-    @EntityScan(basePackages = "guru.springframework.sfgpetclinic.model")
-    static class TestConfig {
+        @Autowired
+        OwnerRepository ownerRepository;
+
+        @EnableJpaRepositories(basePackages = "guru.springframework.sfgpetclinic.repository")
+        @EntityScan(basePackages = "guru.springframework.sfgpetclinic.model")
+        static class TestConfig {
+        }
+
+        @Test
+        void findByLastName() {
+            Owner owner = Owner.builder()
+                .firstName("Max")
+                .lastName("Mustermann")
+                .address("Musterstraße 1")
+                .city("Berlin")
+                .telephone("123456789")
+                .build();
+
+            ownerRepository.save(owner);
+
+            Owner foundOwner = ownerRepository.findByLastName("Mustermann");
+            assertNotNull(foundOwner);
+            assertEquals("Max", foundOwner.getFirstName());
+        }
     }
 
-    @Test
-    void findByLastName() {
-        Owner owner = Owner.builder()
-            .firstName("Max")
-            .lastName("Mustermann")
-            .address("Musterstraße 1")
-            .city("Berlin")
-            .telephone("123456789")
-            .build();
+    @Nested
+    @ActiveProfiles("springdatajpa")
+    @DataJpaTest
+    @ContextConfiguration(classes = {AbstractOwnerServiceTest.TestConfig.class})
+    class JpaTest extends AbstractOwnerServiceTest {
 
-        ownerRepository.save(owner);
+    }
 
-        Owner foundOwner = ownerRepository.findByLastName("Mustermann");
-        assertNotNull(foundOwner);
-        assertEquals("Max", foundOwner.getFirstName());
+    @Nested
+    @ActiveProfiles("map")
+    @DataJpaTest
+    @ContextConfiguration(classes = {AbstractOwnerServiceTest.TestConfig.class})
+    class MapTest extends AbstractOwnerServiceTest {
+
     }
 }
